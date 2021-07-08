db725068c220dc5daa196181a3fb4f398bde3d20
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-08 10:05:12 +0700

    Instead of using mixins with the  generics.GenericAPIView, switched the views to other generics that already have the mixins incorporated. This made the code a lot more concise!

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,37 +1,13 @@
-from rest_framework import mixins, generics
+from rest_framework import generics
 from .models import Snippet
 from .serializers import SnippetModelSerializer
 
 # Create your views here.
-class SnippetList(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    generics.GenericAPIView):
-
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetModelSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView):
-    
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetModelSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
