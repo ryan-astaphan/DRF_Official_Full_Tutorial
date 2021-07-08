5a9132fe6fb2b47bd61f0928e077e063ab258bd7
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-08 10:45:59 +0700

    1) Linked our model with the ForeignKey(User), then updated the serializer and views to reflect this. 2) Added login option to browsable API, 3) Created read/write permissions.

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,38 +1,21 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetModelSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
 
-'''
-Below is an example of a basic Serializer
-'''
-# class SnippetSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-#     code = serializers.CharField(style={'base_template': 'textarea.html'})
-#     lineos = serializers.BooleanField(required=False)
-#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
 
-#     def create(self, validated_data):
-#         '''
-#         Create and return a new "Snippet" instance, given the validated data
-#         '''
-#         return Snippet.objects.create(**validated_data)
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'snippets']
 
-#     def update(self, instance, validated_data):
-#         '''
-#         Update and return an existing "Snippet" instance, given the validated data
-#         '''
-#         instance.title = validated_data.get('title', instance.title)
-#         instance.code = validated_data.get('code', instance.code)
-#         instance.lineos = validated_data.get('lineos', instance.lineos)
-#         instance.language = validated_data.get('language', instance.language)
-#         instance.style = validated_data.get('style', instance.style)
-#         instance.save()
-#         return instance
+        
